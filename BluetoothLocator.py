aa32dfc5e3409ed7a89d7e9e00f9da0876b6158e
Jack <jack.w.sibert@outlook.com>
2024-03-10 18:51:05 -1000

    added distance guessing functionality

--- BluetoothLocator.py
+++ BluetoothLocator.py
@@ -1,19 +1,98 @@
-from bluepy.btle import Scanner
+from bluepy.btle import Scanner, BTLEException
 import dbus
 import subprocess
 import threading
-import time
+import concurrent.futures
+import math
 
 class BluetoothLocator:
     def __init__(self):
-        self.devices = {}
+        self.hci_devices = []
+        self.ble_devices = []
 
-    def scan_for_devices(self):
+        self.unified_dict = {}
+
+
+    def scan_ble_devices(self):
         scanner = Scanner()
-        devices = scanner.scan(10.0)
+        scan_devices = scanner.scan(10.0)
 
-        for device in devices:
+        for device in scan_devices:
             print(f"Device {device.addr} ({device.addrType}), RSSI={device.rssi} dB")
+            device_dict = {
+                "mac": device.addr,
+                "addressType": device.addrType,
+                "RSSI": device.rssi
+            }
+            self.ble_devices.append(device_dict)
+
+    def robust_scan(self):
+        max_attempts = 3
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                return self.scan_ble_devices()
+            except BTLEException as e:
+                attempts += 1
+                print("Retrying scan due to exception: {e}")
+
+
+    def resolve_mac_addresses(self):
+
+        if len(self.hci_devices) < 1 or len(self.ble_devices) < 1:
+            print("There is not enough scan data to perform device resolution.")
+            return
+
+        list_copy = self.ble_devices.copy()
+        temp_list = []
+
+        for dictionary1 in self.hci_devices:
+            match = False
+            for dictionary2 in list_copy:
+                if dictionary1['mac'] == dictionary2['mac']:
+                    match =True
+                    dictionary2['name'] = dictionary1['name']
+            if not match:
+                dict_copy = dictionary1.copy()
+                dict_copy['RSSI'] = 'n/a'
+                dict_copy['addressType'] = 'n/a'
+                temp_list.append(dict_copy)
+        
+        for item in temp_list:
+            list_copy.append(item)
+        
+        for dictionary in list_copy:
+            if 'name' not in dictionary:
+                dictionary['name'] = 'n/a'
+        
+        temp_dict = {}
+        index = 0
+        for dictionary in list_copy:
+            dict_copy = dictionary.copy()
+            temp_key = str(index)
+            temp_dict[temp_key] = dict_copy
+            index += 1
+
+        self.unified_dict = temp_dict
+
+    def rssi_to_distance(self, rssi, tx_power, path_loss_exponent):
+        """
+        Convert RSSI value to distance in meters.
+        
+        :param rssi: The RSSI value in dBm.
+        :param tx_power: The calibrated RSSI value at 1 meter. Typically between -60 and -70 dBm.
+        :param path_loss_exponent: The path loss exponent, depending on the environment.
+        :return: The estimated distance in meters.
+        """
+        if rssi == 0:
+            return -1.0  # distance cannot be calculated
+
+        # Calculate the ratio of the RSSI to the Tx Power
+        ratio = rssi / tx_power
+        # Use the path loss exponent in the distance calculation
+        distance = math.pow(10, (tx_power - rssi) / (10 * path_loss_exponent))
+
+        return distance
 
 
     def get_rssi(self, device_address):
@@ -35,34 +114,36 @@ class BluetoothLocator:
             print(f"Error accessing device properties: {e}")
             return None
     
-    def print_scanning_message(self, stop_event):
-        dot_count = 0
-        while not stop_event.is_set():
-            print(f'\rScanning for devices{"." * dot_count}', end ='')
-            dot_count += 1
-            time.sleep(1)
-        print()
         
     def scan_devices(self):
         """
         Scans for Bluetooth devices using hcitool and returns a list of tuples containing MAC addresses and device names.
         """
-        stop_message = threading.Event()
-        message_thread = threading.Thread(target=self.print_scanning_message, args=(stop_message,))
-        message_thread.start()
 
         try:
             scan_output = subprocess.check_output(['hcitool', 'scan'], encoding='utf-8')
             # Process the output to extract device information
             lines = scan_output.split('\n')[1:]  # First line is the header, so skip it
+            
             for line in lines:
+                new_dict = {}
                 parts = line.split('\t')
                 if len(parts) >= 2:
-                    mac_address = parts[1]
-                    name = parts[2]
-                    self.devices[name] = mac_address
+                    new_dict['mac'] = str(parts[1]).lower()
+                    new_dict['name'] = parts[2]
+                    self.hci_devices.append(new_dict)
         except subprocess.CalledProcessError as e:
             print(f"Failed to scan Bluetooth devices: {e}")
+    
+    def run_in_parallel(*funcs):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = [executor.submit(func) for func in funcs]
+            for future in concurrent.futures.as_completed(results):
+                try:
+                    print("Executing HCI and BLE scans...")
+                    thread_result = future.result()
+                except Exception as e:
+                    print(f"Generated an exception: {e}")
         
-        stop_message.set()
-        message_thread.join()
+
+
--- main.py
+++ main.py
@@ -1,17 +1,78 @@
+import re
 # main.py
 
 from BluetoothLocator import BluetoothLocator
 
+def is_valid_mac_address(mac):
+    pattern = re.compile(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
+    return bool(pattern.match(mac))
+
 def main():
     locator = BluetoothLocator()
-    locator.scan_for_devices()
-
+    # locator.scan_ble_devices()
     # locator.scan_devices()
-    # print("The scanned devices are: ", locator.devices)
-    # n_device = 0
-    # if len(locator.devices) > 0:
+
+    locator.run_in_parallel(locator.scan_devices, locator.robust_scan) # calls the scan_devices() and scan_ble_devices() functions and runs them in parallel
+    print("The scanned devices are: ", locator.hci_devices)
+    print("The scanned ble devices are: ", locator.ble_devices)
+    locator.resolve_mac_addresses()
+    
+    
+    n_device = 0
+    if(len(locator.unified_dict)) > 0:
+        print("\nUsing the corresponding number, select desired device to connect to:")
+        print("\n")
+        print("Selector\t" + "MAC\t" + "Address Type\t" + "RSSI(db)\t" + "Name")
+        for key, value in locator.unified_dict.items():
+            output_string = str(key) + "\t"
+            for key2, value2 in value.items():
+                output_string = output_string + str(value2) + "\t"
+            print(output_string)
+            output_string = ""
+        user_selection = input("Provide a device Selector # or type 'Other' to attempt to connect to another device: ")
+        
+        mac_selection = ""
+        if str(user_selection).lower() != 'other':
+            try:
+                int_selection = int(user_selection)
+                if not (0 <= int_selection < len(locator.unified_dict)):
+                    print("Invalid selection, terminating.")
+                    exit()
+            except ValueError:
+                print("Please input a valid integer value corresponding to the device you wish to connect to.")
+            
+            mac_selection = locator.unified_dict[user_selection]['mac']
+
+        else:
+            mac_selection = input("Please provide another MAC in the format of XX:XX:XX:XX:XX:XX \n")
+            mac_selection = mac_selection.lower()
+            if is_valid_mac_address(mac_selection):
+                print("Provided MAC address: ", mac_selection)
+
+            else:
+                print("Invalid MAC input, please ensure MAC is in format of XX:XX:XX:XX:XX:XX:XX\nExiting...")
+                exit()
+        
+        print("This is the MAC address you selected: ", mac_selection)
+
+        rssi = locator.unified_dict[user_selection]['RSSI']
+        tx_power = -46
+        path_loss_exponent = 2 # indoor environment will typically have between 2 and 4 for this value
+
+        offset_dist = locator.rssi_to_distance(rssi, tx_power, path_loss_exponent)
+        print(f"This device is approx. {str(offset_dist)} away")
+
+
+
+    else:
+        print("No BT devices detected, exiting.")
+        exit()
+
+
+
+    # if len(locator.hci_devices) > 0:
     #     print("Using the corresponding number, select desired device to connect to.")
-    #     for key, value in locator.devices.items():
+    #     for key, value in locator.hci_devices.items():
     #         print(str(n_device) +"\t" + key + "\t" + value)
     #         n_device += 1
     #     user_selection = input("Device #: ")
@@ -23,7 +84,7 @@ def main():
     #             exit()
             
     #         # MAC address of chosen device
-    #         for i, (key, value) in enumerate(locator.devices.items()):
+    #         for i, (key, value) in enumerate(locator.hci_devices.items()):
     #             if i == int_selection:
     #                 print("True, int_selection: ", int_selection)
     #                 print("device_address: ", value)
